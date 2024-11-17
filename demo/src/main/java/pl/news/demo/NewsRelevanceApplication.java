f533248f915a63cabd3abb81f6fedde7e0ddbce3
Ernest Strychalski <strychalski.ernest@gmail.com>
2024-11-17 14:27:09 +0100

    Clean maven spring boot project

---
+++ demo/src/main/java/pl/news/demo/NewsRelevanceApplication.java
@@ -0,0 +1,13 @@
+package pl.news.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsRelevanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsRelevanceApplication.class, args);
+	}
+
+}
